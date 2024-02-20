5c1073684afa8a8fb52d56d50de1bddf525a2061
Leo-Turible <turibleleo@gmail.com>
2024-02-20 08:27:40 +0100

    search in real time

--- src/views/Pays.vue
+++ src/views/Pays.vue
@@ -4,16 +4,28 @@ import axios from 'axios'
 import CardPays from '@/components/CardPays.vue'
 
 const data = ref([])
+const listeComplete = ref([])
+const recherche = ref('')
 
 onMounted(async () => {
   const response = await axios.get('https://restcountries.com/v3.1/all')
-  data.value = response.data
+  listeComplete.value = response.data
+  data.value = [...listeComplete.value]
 })
+
+const filtrer = () => {
+  data.value = listeComplete.value.filter(pays => pays.name.common.toLowerCase().includes(recherche.value.toLowerCase()))
+}
+
 </script>
 
 <template>
   <div id="pays-page">
     <h1>Pays</h1>
+    <input type="text" v-model="recherche" @input="filtrer" placeholder="Rechercher un pays">
+    <!--<button @click="filtrer">Filtrer</button>-->
+    <!--<button @click="reset">Reset</button>-->
+
     <div id="cards-container">
       <div v-for="pays in data" :key="pays.cca3">
         <CardPays :pays="pays" />
