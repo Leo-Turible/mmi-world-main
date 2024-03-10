17653089f7f1d25079a98069a7ae21568a8cbb72
Leo-Turible <turibleleo@gmail.com>
2024-03-10 22:38:44 +0100

    More informations

--- src/views/Fournisseurs.vue
+++ src/views/Fournisseurs.vue
@@ -4,6 +4,10 @@ import axios from 'axios'
 
 const data = ref()
 const libelle = ref()
+const rue = ref()
+const codepostal = ref()
+const ville = ref()
+const id = ref('')
 const error = ref()
 
 onMounted(async () => {
@@ -16,19 +20,40 @@ onMounted(async () => {
 })
 
 async function ajouterFournisseur() {
-  if (!libelle.value) {
-    error.value = 'Le libellé est requis.'
+  if (!libelle.value || !rue.value || !codepostal.value || !ville.value) {
+    error.value = 'Tous les champs sont requis.'
     return
+  } else {
+    error.value = ''
   }
 
   try {
     const response = await axios.post('http://symfony.mmi-troyes.fr:8313/api/fournisseurs', {
-      libelle: libelle.value
+      libelle: libelle.value,
+      adresse: {
+        rue: rue.value,
+        codepostal: codepostal.value,
+        ville: ville.value
+      }
     })
     data.value['hydra:member'].push(response.data)
+    data.value['hydra:totalItems']++
     libelle.value = ''
+    rue.value = ''
+    codepostal.value = ''
+    ville.value = ''
   } catch (err) {
     error.value = 'Une erreur est survenue lors de l\'ajout du fournisseur.'
+  }
+}
+
+async function supprimer(id) {
+  try {
+    await axios.delete(`http://symfony.mmi-troyes.fr:8313/api/fournisseurs/${id}`)
+    data.value['hydra:member'] = data.value['hydra:member'].filter(fournisseur => fournisseur.id !== id)
+    data.value['hydra:totalItems']--
+  } catch (err) {
+    error.value = 'Une erreur est survenue lors de la suppression du fournisseur.'
   }
 }
 
@@ -37,22 +62,32 @@ async function ajouterFournisseur() {
 <template>
 
   <div>
-    <input type="text" v-model="libelle">
-    <button @click="ajouterFournisseur">Ajouter</button>
+    <form @submit.prevent="ajouterFournisseur">
+      <input type="text" v-model="libelle" placeholder="Libellé">
+      <input type="text" v-model="rue" placeholder="Rue">
+      <input type="text" v-model="codepostal" placeholder="Code Postal">
+      <input type="text" v-model="ville" placeholder="Ville">
+      <button type="submit">Ajouter</button>
+    </form>
     <p v-if="error">{{ error }}</p>
   </div>
+
+
   <div v-if="data">
     <h1>Fournisseurs</h1>
     <strong>Il y a {{ data['hydra:totalItems'] }}</strong>
-    <ul v-for="fournisseur in data['hydra:member']">
-      <li >
-        Libellé : {{ fournisseur.libelle }}
-      </li>
+    <ul v-for="fournisseur in data['hydra:member']" :key="fournisseur.id">
       <li>
-        Adresse : {{ fournisseur.adresse }}
-      </li>
-      <li>
-        Code postal : {{ fournisseur.codePostal }}
+        <strong>Libellé : {{ fournisseur.libelle }}</strong>
+        <p v-if="fournisseur.adresse">
+          Rue : {{ fournisseur.adresse.rue }} <br>
+          Code postal : {{ fournisseur.adresse.codepostal }} <br>
+          Ville : {{ fournisseur.adresse.ville }}
+        </p>
+        <p v-else>
+          Pas d'adresse renseignée.
+        </p>
+        <button @click="supprimer(fournisseur.id)">Supprimer</button>
       </li>
     </ul>
   </div>
